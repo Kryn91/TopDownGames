d3446c495c16bdb2c9302dda528d31048567d24c
kryn91 <keryanalvesdaniel@gmail.com>
2025-10-08 15:09:16 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,77 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include "player.cpp"
 #include <cmath>
 #include <vector>
-
-//player
-struct player
-{
-	sf::RectangleShape body;
-	float speed = 250.f;
-	
-	player()
-       	{
-		body.setSize({50,50});
-		body.setOrigin({25.f, 25.f});
-		body.setFillColor(sf::Color::White);
-	}
-	void setPosition(const sf::Vector2f& pos)
-	{
-		body.setPosition(pos);
-	}
-	sf::Vector2f getPosition(void) const
-	{
-		return(body.getPosition());
-	}
-	sf::FloatRect getBounds() const
-	{
-		return (body.getGlobalBounds());
-	}
-	void updatePosition(float dt, const std::vector<sf::RectangleShape>& walls)
-	{
-	//mouvements
-		sf::Vector2f dir{0.f,0.f};
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z))
-       			dir.y -= 1;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-	       		dir.y += 1;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
-	       		dir.x -= 1;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-       			dir.x += 1;
-	//normlisation(vitesse constante)
-		float len = std::sqrt(dir.x * dir.x + dir.y * dir.y);
-		if (len > 0)
-			dir /= len;
-		sf::Vector2f movement = dir * speed * dt;
-
-	//deplacement sur X
-		body.move({movement.x, 0.f});
-		for (const auto& wall : walls)
-		{
-			if (getBounds().findIntersection(wall.getGlobalBounds()))
-			{
-				body.move({-movement.x, 0.0f});
-				break;
-			}
-		}
-	//deplacement sur Y
-		body.move({0.f, movement.y});
-		for (const auto &wall : walls)
-		{
-			if(getBounds().findIntersection(wall.getGlobalBounds()))
-			{
-				body.move({0.f, -movement.y});
-			break;
-			}
-		}
-	}
-
-	void draw(sf::RenderTarget& target)
-	{
-		target.draw(body);
-	}
-};
 
 int main(void)
 {
@@ -100,6 +30,7 @@ int main(void)
 	sf::View camera;
 	camera.setSize(sf::Vector2f(static_cast<float>(window.getSize().x), static_cast<float>(window.getSize().y)));
 	camera.setCenter(player.getPosition());
+
 
 //temp en jeux
 	sf::Clock clock;
