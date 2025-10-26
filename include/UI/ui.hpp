271e57e45b12183d6c997556341bdf1269ee9299
kryn91 <keryanalvesdaniel@gmail.com>
2025-10-26 19:15:24 +0100

    damagePopup not implement in game

---
+++ include/UI/ui.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class DamagePopup
+{
+private:
+	sf::Text text;
+	sf::Vector2f velocity;
+	float lifeTime = 1.f;
+
+public:
+	DamagePopup(const sf::Font& font, const std::string& str, sf::Vector2f position);
+
+	void	update(float dt);
+	void	render(sf::RenderWindow& window);
+	bool	isAlive() const;
+};
---
+++ src/UI/ui.cpp
@@ -0,0 +1,35 @@
+#include "UI/ui.hpp"
+
+DamagePopup::DamagePopup(const sf::Font& font, const std::string& str, sf::Vector2f position)
+	: text (font, str, 24)
+{
+	text.setFillColor(sf::Color::Red);
+	text.setPosition(position);
+	text.setOutlineColor(sf::Color::Black);
+	text.setOutlineThickness(2);
+
+	velocity = {0.f, -50.f};
+}
+
+void	DamagePopup::update(float dt)
+{
+
+	text.move(velocity * dt);
+	
+	lifeTime -= dt;
+
+	sf::Color color = text.getFillColor();
+	if (lifeTime < 0.5f && color.a > 5)
+		color.a -= static_cast<uint8_t>(200 * dt);
+	text.setFillColor(color);
+}
+
+void	DamagePopup::render(sf::RenderWindow& window)
+{
+		window.draw(text);
+}
+
+bool	DamagePopup::isAlive() const
+{
+	return (lifeTime > 0.f);
+}
